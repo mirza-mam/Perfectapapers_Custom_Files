8e8a8c70dd41881115972e84b1f64c046ebc566a
Muhammad Abdullah Mirza <61614849+mirza-mam@users.noreply.github.com>
2022-03-10 01:28:20 +0500

    Update connection.php

--- includes/connection.php
+++ includes/connection.php
@@ -1,36 +1,29 @@
 <?php
 
-	class connection{
-		
-		private $con;
-		private $con_last_id;
-		
-		public function connect_db(){
-			
-			/* For Localhost */
-		$this->con	= new MySQLi('localhost','root','','perfecta_custom_db');
-			/* For Live DB  */
-		// $this->con	= new MySQLi('localhost','perfecta_admin','n;Go%cU4yVx{','perfecta_custom_db');
-			
-			if( $this->con )
-			{	
-				return $this->con;
-			}
-			else{
-				die("Error in connecting with the Database" . mysqli_connect_error());
-				$this->con->close();
-			}
-				
-	
-					}
-		
-		//When this Function is executed after mysqli_query() it automatically Fetches the last inserted ID :)
-		public function get_last_inserted_id(){
-			
-			 $this->con_last_id = $this->con->insert_id;
-			 return $this->con_last_id;
-			
-					}
+class connection
+{
+
+	private $con;
+	private $con_last_id;
+
+	public function connect_db()
+	{
+		/* For Localhost */
+		// $this->con	= new MySQLi('localhost','root','','perfecta_custom_db');
+		/* For Live DB  */
+		$this->con	= new MySQLi('localhost', 'perfect2_custom_admin', '&KL={u_Ic6;F', 'perfect2_custom_db');
+		if ($this->con) {
+			return $this->con;
+		} else {
+			die("Error in connecting with the Database" . mysqli_connect_error());
+			$this->con->close();
+		}
 	}
 
-?>
+	//When this Function is executed after mysqli_query() it automatically Fetches the last inserted ID :)
+	public function get_last_inserted_id()
+	{
+		$this->con_last_id = $this->con->insert_id;
+		return $this->con_last_id;
+	}
+}
