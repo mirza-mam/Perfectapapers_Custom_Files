688e6bbe6637e7397ac8125a070d76125853e840
Muhammad Abdullah Mirza <61614849+mirza-mam@users.noreply.github.com>
2022-05-31 20:22:36 +0500

    Update connection.php

--- includes/connection.php
+++ includes/connection.php
@@ -11,7 +11,7 @@ class connection
 		/* For Localhost */
 		//  $this->con	= new MySQLi('localhost','root','','perfecta_custom_db');
 		/* For Live DB  */
-		$this->con	= new MySQLi('localhost', 'perfect2_custom_admin', '&KL={u_Ic6;F', 'perfect2_custom_db');
+		$this->con = new MySQLi('localhost', 'perfect2_custom_admin', '&KL={u_Ic6;F', 'perfect2_custom_db');
 		if ($this->con) {
 			return $this->con;
 		} else {
